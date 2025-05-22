857525e9e269848ab4fcc81f9a8e7e364a5bb734
dkhal <dkhalimov2012@gmail.com>
2025-05-22 18:02:48 +0300

    ferrrrr

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import arcade
 from arcade import Rect
 
 
-SQUARE_SCALE = 75
+SQUARE_SCALE = 60
 ROW_AMOUNT = 10
 COLUMN_AMOUNT = 11
 SCREEN_WIDTH = COLUMN_AMOUNT * SQUARE_SCALE
@@ -31,7 +31,15 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         self.clear()
-        arcade.draw_rect_filled(Rect(left=0,right=100,bottom=0,top=250,width= 100,height=100,x=0,y=0),color="green")
+        for y in range (ROW_AMOUNT):
+            for x in range (COLUMN_AMOUNT):
+                if (x  % 2 == 0 and y % 2 == 0) or (x % 2 == 1 and y % 2 == 1):
+                    arcade.draw_rect_filled(Rect(left=0, right=0, bottom=0, top=0, width= SQUARE_SCALE, height=SQUARE_SCALE,
+                                                 x=x * SQUARE_SCALE + SQUARE_SCALE / 2, y=y * SQUARE_SCALE + SQUARE_SCALE / 2), color=(45, 214, 17))
+                else:
+                    arcade.draw_rect_filled(Rect(left=0, right=0, bottom=0, top=0, width= SQUARE_SCALE, height=SQUARE_SCALE,
+                                                 x=x * SQUARE_SCALE + SQUARE_SCALE / 2, y=y * SQUARE_SCALE + SQUARE_SCALE / 2), color=(32, 156, 12))
+
     def on_update(self, delta_time):
         pass
 
