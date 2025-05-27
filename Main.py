ec08e49eef9b74027e8402aa1683c9fdbbae1b4e
dkhal <dkhalimov2012@gmail.com>
2025-05-27 16:00:32 +0300

    ferrrrr

--- Main.py
+++ Main.py
@@ -3,11 +3,25 @@ from arcade import Rect
 
 
 SQUARE_SCALE = 60
-ROW_AMOUNT = 10
+ROW_AMOUNT = 11
 COLUMN_AMOUNT = 11
 SCREEN_WIDTH = COLUMN_AMOUNT * SQUARE_SCALE
 SCREEN_HEIGHT = ROW_AMOUNT * SQUARE_SCALE
-SCREEN_TITLE = "My snake"
+SCREEN_TITLE = "My snake game"
+
+class SpriteSnake(arcade.Sprite):
+    def __init__(self):
+        super().__init__("sprite (1).png",scale=0.22,center_x=1*SQUARE_SCALE+SQUARE_SCALE/2,center_y=5*SQUARE_SCALE+SQUARE_SCALE/2)
+        self.where_snake_fasing = 2
+    def WhereSnakeFasing(self):
+        if self.where_snake_fasing == 1:
+            self.angle = 90
+        if self.where_snake_fasing == 2:
+            self.angle = 180
+        if self.where_snake_fasing == 3:
+            self.angle = -90
+        if self.where_snake_fasing == 4:
+            self.angle = 0
 
 class MyGame(arcade.Window):
     def __init__(self):
@@ -23,7 +37,10 @@ class MyGame(arcade.Window):
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
         ]
+        self.snakehead = SpriteSnake()
+
 
 
     def setup(self):
@@ -40,12 +57,21 @@ class MyGame(arcade.Window):
                     arcade.draw_rect_filled(Rect(left=0, right=0, bottom=0, top=0, width= SQUARE_SCALE, height=SQUARE_SCALE,
                                                  x=x * SQUARE_SCALE + SQUARE_SCALE / 2, y=y * SQUARE_SCALE + SQUARE_SCALE / 2), color=(32, 156, 12))
 
+        arcade.draw_sprite(self.snakehead)
+
     def on_update(self, delta_time):
         pass
 
     def on_key_press(self, key, modifiers):
-        pass
-
+        if key == arcade.key.UP:
+            self.snakehead.where_snake_fasing = 1
+        if key == arcade.key.RIGHT:
+            self.snakehead.where_snake_fasing = 2
+        if key == arcade.key.DOWN:
+            self.snakehead.where_snake_fasing = 3
+        if key == arcade.key.LEFT:
+            self.snakehead.where_snake_fasing = 4
+        self.snakehead.WhereSnakeFasing()
     def on_key_release(self, key, modifiers):
         pass
 
